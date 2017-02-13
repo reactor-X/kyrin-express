8efd9a10654dae9f8fce96639682b7324f52a895
Ravi Semwal <ravisemwal94@outlook.com>
2017-02-13 11:43:54 +0530

    Cleanup

--- src/controllers/home/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var express = require("express");
-var router = express.Router();
-var kyrin_1 = require("../../../kyrin");
-router.get('/', function (req, res, next) {
-    res.render('index', { service_result: kyrin_1.default.get('say-hello').sayHello() });
-});
-module.exports = router;
