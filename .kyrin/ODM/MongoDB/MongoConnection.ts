35860ee6db824d5d05e8f3a344784b3faa18fb1f
Ravi Semwal <ravisemwal94@outlook.com>
2017-03-07 12:25:46 +0530

    Added conditional injection of container, defining container as null will automatically inject it.

--- .kyrin/ODM/MongoDB/MongoConnection.ts
+++ .kyrin/ODM/MongoDB/MongoConnection.ts
@@ -1,10 +1,10 @@
 import mongoose = require("mongoose");
-import customPromise= require("bluebird");
+import customPromise = require("bluebird");
 export default class MongoConnection {
     private static connection;
     private static logger;
     constructor(connectionConfig, logger) {
-        mongoose.Promise=customPromise;
+        mongoose.Promise = customPromise;
         MongoConnection.logger = logger;
         mongoose.connection.on('error', function () { MongoConnection.logger.kErr('Unable to connect to mongo instance. Please, check if mongo is running and configuration is correct.') });
         mongoose.connection.once('open', function () {
--- src/services/Demo/demo.ts
+++ src/services/Demo/demo.ts
@@ -1,8 +1,6 @@
-
 class Demo {
-    public container;
+    public container = null;
     public sayHello() {
-        console.log(this.container);
         return "Hello from the simple Demo service. You can find it as 'say-hello' in services.yml!";
     }
 }
